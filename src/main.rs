b9eb2c520c26a5e52e90bd96690527a731b6e127
youxuanzhao <youxuanzhao2007@outlook.com>
2026-03-04 08:37:41 -0800

    added dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use bevy::prelude::*;
+use mlua::prelude::*;
+use rig::client::{CompletionClient, ProviderClient};
+use rig::completion::Prompt;
+use rig::providers::openai;
+
 fn main() {
     println!("Hello, world!");
 }
